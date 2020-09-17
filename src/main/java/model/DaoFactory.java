52ea4574e79b4a042ccc7c8c9d2273657e5ceddf
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-17 16:45:37 -0500

    organizing interface variables

--- src/main/java/model/DaoFactory.java
+++ src/main/java/model/DaoFactory.java
@@ -8,9 +8,12 @@ public class DaoFactory {
     // We don't want to require accessing the BurgersDAO directly,
     //  instead we will reference its interface
     private static Burgers burgersDao;
+    private static Ingredients ingredientsDao;
+    private static Sodas sodasDao;
 
     // Any Bean type we have will get a method called:
     //  get[Bean]Dao()
+
     public static Burgers getBurgersDao (){
         if (burgersDao == null) {
             burgersDao = new BurgersDAO();
@@ -18,16 +21,12 @@ public class DaoFactory {
         return burgersDao;
     }
 
-    private static Ingredients ingredientsDao;
-
     public static Ingredients getIngredientsDao() {
         if (ingredientsDao == null) {
             ingredientsDao = new IngredientsDAO();
         }
         return ingredientsDao;
     }
-
-    private static Sodas sodasDao;
 
     public static Sodas getSodasDao() {
         if (sodasDao == null) {
